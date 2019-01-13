8699d3805f8d67ff214855a835c5c90567c0d8a3
YutaKanna <kannayuta0629@gmail.com>
2019-01-13 11:26:13 +0900

    前回とは異なるじゃんけんの結果が表示されるように変更する

--- rock-paper-scissors/ViewController.swift
+++ rock-paper-scissors/ViewController.swift
@@ -24,24 +24,38 @@ class ViewController: UIViewController {
     
     @IBAction func shuffleAction(_ sender: Any) {
         
-      // 0,1,2の数値をランダムに算出(乱数)
-      // arc4random_uniform()の戻り値はUInt32だがSwiftの標準的な整数型Intにキャスト(変換)する
-      answerNumber = Int(arc4random_uniform(3))
+        // 新しいじゃんけんの結果を一時的に格納する変数を設ける
+        var newAnswerNumber = 0
         
-      if answerNumber == 0 {
+        // ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+        // repeat は繰り返しを意味する
+        repeat {
+            
+            // 0,1,2の数値をランダムに算出(乱数)
+            // arc4random_uniform()の戻り値はUInt32だがSwiftの標準的な整数型Intにキャスト(変換)する
+            newAnswerNumber = Int(arc4random_uniform(3))
+            
+            // 前回と同じ結果の時は、再度、ランダムに数値を出す
+            // 異なる結果の時は、repeatを設ける
+        } while answerNumber == newAnswerNumber
+        
+        // 新しいじゃんけんの結果を格納
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0 {
         // グー
         answerLabel.text = "グー"
         answerImageView.image = UIImage(named: "gu")
-        
-      } else if answerNumber == 1 {
+
+        } else if answerNumber == 1 {
         // チョキ
         answerLabel.text = "チョキ"
         answerImageView.image = UIImage(named: "choki")
-      } else if answerNumber == 2 {
+        } else if answerNumber == 2 {
         // パー
         answerLabel.text = "パー"
         answerImageView.image = UIImage(named: "pa")
-      }
+        }
 
     }
 }
